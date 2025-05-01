e5885370b33eaad386c815dc65fa2c20a55bf870
ABSOLUTE-NOTHING <116956963+ABSOLUTE-NOTHING@users.noreply.github.com>
2025-05-01 17:33:22 +0530

    minor UI changes, (footer and page title)

--- src/app/vm/layout.tsx
+++ src/app/vm/layout.tsx
@@ -1,6 +1,7 @@
 import { VendingMachineProvider } from "@/context/VendingMachineContext";
 import React, { ReactNode } from "react";
 
+
 const layout = ({ children }: { children: ReactNode }) => {
   return <VendingMachineProvider>{children}</VendingMachineProvider>;
 };
